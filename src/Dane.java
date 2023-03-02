560cdeda1aade1087da99e12d598b0420e5ccb65
dane7184 <dane7184@gmail.com>
2023-03-02 11:42:14 +0700

    add

--- src/Dane.java
+++ src/Dane.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class Dane {
     public static void main(String[] args) {
+        System.out.println("Hello");
         System.out.println("Please add Homework here ");
         System.out.println("Hello everyone");
-
     }
 }
