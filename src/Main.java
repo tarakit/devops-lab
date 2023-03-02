ffd02d4265155f2851a37193b84da1c20f22e227
dane7184 <dane7184@gmail.com>
2023-03-02 11:08:16 +0700

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        int age =20;
+        System.out.println("My age = "+ age);
         System.out.println("Hi DevOps Class");
     }
 }
