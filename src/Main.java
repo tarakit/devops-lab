9170b18433ae904cf989d31e2d6c640b56cc46da
SopheaKoy <koysophea09@gamil.com>
2023-03-02 11:06:18 +0700

    phea

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
 
         System.out.println("Hi DevOps Class");
         Sophea.Miss();
-=======
-        int i=0;
+
+        int i = 0;
         System.out.println("Hi DevOps Class");
         System.out.println("Hello world");
->>>>>>> 660a1da31b0d98e3d15db7e25ef51b3282a16487
     }
 }
