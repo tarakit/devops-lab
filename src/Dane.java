fe9ebef16dac8fd8085d517bc67f12f8a60a92ef
dane7184 <dane7184@gmail.com>
2023-03-02 11:55:12 +0700

    add

--- src/Dane.java
+++ src/Dane.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Dane {
     public static void main(String[] args) {
         int age=20;
-
+        String name= "Dane";
         System.out.println("Hello");
         System.out.println("Please add Homework here ");
         System.out.println("Hello everyone");
